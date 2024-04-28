104384a5e34348c660105522cd62e4a6207e8063
Prathamesh <prathameshdubbewar@gmail.com>
2024-04-28 12:56:22 +0530

    D

---
+++ ExceptionHandling3/1_product_ValueError.py
@@ -0,0 +1,17 @@
+def calc_product(numbers):
+    product = 1
+    for num in numbers:
+        product *= num
+    return product
+
+try:
+    input_string = input("Enter a list of integers: ")
+
+    numbers = [int(num) for num in input_string.split(',')]
+
+    product = calc_product(numbers)
+
+    print("Product of the numbers:", product)
+
+except ValueError:
+    print("Error: Input is not int")
